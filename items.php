430fb6ba3f0b29c0d61289d82bb59cb2116d02e5
tuazonandreijames-dot <tuazonandreijames@gmail.com>
2025-11-29 22:55:59 +0800

    Add files via upload

---
+++ items.php
@@ -0,0 +1,34 @@
+<?php
+$items = [
+    [
+        "name" => "Nintendo Entertainment System (NES)", 
+        "price" => 5500, 
+        "stock" => 3, 
+        "category" => "Console"
+    ],
+    [
+        "name" => "Sega Genesis Model 2", 
+        "price" => 3500, 
+        "stock" => 5, 
+        "category" => "Console"
+    ],
+    [
+        "name" => "Super Mario World (SNES Cartridge)", 
+        "price" => 1500, 
+        "stock" => 0, // Out of Stock
+        "category" => "Game"
+    ],
+    [
+        "name" => "Original Game Boy (Grey)", 
+        "price" => 4000, 
+        "stock" => 1, // Low Stock
+        "category" => "Handheld"
+    ],
+    [
+        "name" => "PlayStation 1 (PSX)", 
+        "price" => 4500, 
+        "stock" => 10, 
+        "category" => "Console"
+    ],
+];
+?>
